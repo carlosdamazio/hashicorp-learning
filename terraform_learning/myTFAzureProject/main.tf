43ac0ea69c2c69e41ee960c6c54ecf1d941a1055
Carlos Damázio <kaduardobsb95@gmail.com>
2020-08-06 22:45:03 -0300

    adding tfvars

--- terraform_learning/myTFAzureProject/main.tf
+++ terraform_learning/myTFAzureProject/main.tf
@@ -1,25 +1,22 @@
 # Resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
-    location = "brazilsouth"
-
-    tags = {
-        Environment = "Terraform Getting Started again"
-        Team = "DevOps"
-    }
+    name     = "${var.resource_prefix}TFResourceGroup"
+    location = var.location
+    tags     = var.tags
 }
 
 # Virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
+    name                = "${var.resource_prefix}TFVnet"
     address_space       = ["10.0.0.0/16"]
-    location            = "brazilsouth"
+    location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
+    tags                = var.tags
 }
 
 # Subnet
 resource "azurerm_subnet" "subnet" {
-    name                 = "myTFSubnet"
+    name                 = "${var.resource_prefix}TFSubnet"
     resource_group_name  = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes     = ["10.0.1.0/24"]
@@ -27,17 +24,19 @@ resource "azurerm_subnet" "subnet" {
 
 # Public IP
 resource "azurerm_public_ip" "publicip" {
-    name                = "myTFPublicIP"
+    name                = "${var.resource_prefix}TFPublicIP"
     location            = "brazilsouth"
     resource_group_name = azurerm_resource_group.rg.name
-    allocation_method   = "Static"
+    allocation_method   = "Dynamic"
+    tags                = var.tags
 }
 
 # Network Security Group and its rules
 resource "azurerm_network_security_group" "nsg" {
-    name                = "myTFNSG"
-    location            = "brazilsouth"
+    name                = "${var.resource_prefix}TFNSG"
+    location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
+    tags                = var.tags
 
     security_rule {
         name                       = "SSH"
@@ -54,12 +53,13 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Network interface
 resource "azurerm_network_interface" "nic" {
-    name                = "myNIC"
-    location            = "brazilsouth"
+    name                = "${var.resource_prefix}NIC"
+    location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
+    tags                = var.tags
 
     ip_configuration {
-        name                          = "myNICConfig"
+        name                          = "${var.resource_prefix}NICConfig"
         subnet_id                     = azurerm_subnet.subnet.id
         private_ip_address_allocation = "dynamic"
         public_ip_address_id          = azurerm_public_ip.publicip.id
@@ -74,11 +74,12 @@ resource "azurerm_network_interface_security_group_association" "nic-nsg" {
 
 # Linux virtual machine
 resource "azurerm_virtual_machine" "vm" {
-    name                  = "myTFVM"
-    location              = "brazilsouth"
+    name                  = "${var.resource_prefix}TFVM"
+    location              = var.location
     resource_group_name   = azurerm_resource_group.rg.name
     network_interface_ids = [azurerm_network_interface.nic.id]
     vm_size               = "Standard_DS1_v2"
+    tags                  = var.tags
 
     storage_os_disk {
         name              = "myOsDisk"
@@ -95,12 +96,23 @@ resource "azurerm_virtual_machine" "vm" {
     }
 
     os_profile {
-        computer_name  = "myTFVM"
-        admin_username = "damnazio"
-        admin_password = "goF1gure"
+        computer_name  = "${var.resource_prefix}TFVM"
+        admin_username = var.admin_username
+        admin_password = var.admin_password
     }
 
     os_profile_linux_config {
         disable_password_authentication = false
     }
+
+}
+
+data "azurerm_public_ip" "ip" {
+    name                = azurerm_public_ip.publicip.name
+    resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+    depends_on = [azurerm_virtual_machine.vm]
+}
+
+output "os_sky" {
+    value = lookup(var.sku, var.location)
 }
---
+++ terraform_learning/myTFAzureProject/terraform.tfvars
@@ -0,0 +1,4 @@
+location = "brazilsouth"
+resource_prefix = "dmz"
+admin_username = "damnazio"
+admin_password = "geL3ia"
